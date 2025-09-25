c4232075980fe7af7cb7bba1286c7667bac7041c
BLACK._.CORVUS <2676amirhossein2676@gmail.com>
2025-09-25 14:25:05 +0330

    Test LinearRegression model

---
+++ S1_E2.py
@@ -0,0 +1,35 @@
+# Test LinearRegression model 
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import matplotlib.pyplot as plt
+
+F = lambda x : 4 * x + 2
+data = 50
+x = np.linspace(-10,10,data)
+
+y = F(x)
+
+Nois = np.random.randn(data) * 5
+y_nois = y + Nois
+
+x = x.reshape(-1,1)
+y = y.reshape(-1,1)
+y_nois = y_nois.reshape(-1,1)
+
+modle = LinearRegression() 
+modle.fit(x,y_nois)
+
+g = lambda x : modle.coef_ * x + modle.intercept_
+Y_predicted = g(x)
+plt.plot(x,y,"sg")
+plt.plot(x,y_nois,"ob")
+plt.plot(x,Y_predicted,"-r")
+plt.legend(["Orginal data ", "Noise data" ,"Predicted"])
+plt.title(f"y = 4 * x + 2 Y_hat {modle.coef_} * x {modle.intercept_}")
+
+plt.show()
+
+
+
+
+
