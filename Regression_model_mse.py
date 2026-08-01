9b560c700ebc6139d88951b57f8ba3039847ee2f
BLACK._.CORVUS <2676amirhossein2676@gmail.com>
2026-08-01 16:56:04 +0330

    Regression_model_MSE

---
+++ Regression_model_mse.py
@@ -0,0 +1,40 @@
+#_____Regression_model_MSE____
+import numpy as np 
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+def f(x):
+    return 2 * x + 5
+
+def g(a, b, x):
+    return a * x + b
+
+# Create Data
+X = np.linspace(0,100,101) 
+Y = f(X)
+
+#Data reshape
+X = X.reshape(101,1)
+Y = Y.reshape(101,1)
+Y_new = Y + np.random.randn(101, 1)* 100 #Create Data with noise
+
+#Fit model
+model =LinearRegression()
+model.fit(X,Y_new)
+a_new = model.coef_ # A
+b_new = model.intercept_ # B
+#predcit model
+Y_predicted = model.predict(X) # or
+# Y_new = g(a_new, b_new, X)
+
+ms = mean_squared_error(Y_predicted, Y_new)
+
+print(f"A = {a_new} B = {b_new}")
+plt.plot(X, Y, "b-")
+plt.plot(X, Y_new, "ro")
+plt.plot(X, Y_predicted, "g-")
+plt.title(f"y ={a_new} * x + {b_new} ")
+plt.legend(["Real line","Real Data", "Predicted Line"])
+plt.show()
+
