e36e844f1d599f216807f14183ce579b8aa75b8d
BLACK._.CORVUS <2676amirhossein2676@gmail.com>
2026-08-14 13:59:46 +0330

    Regression model Train and Test with mean squared error

--- Regression_model_Train_Test.py
+++ Regression_model_Train_Test.py
@@ -1,4 +1,4 @@
-
+# Regression model Train and Test with mean squared error
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
